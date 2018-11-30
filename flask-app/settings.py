903719e5a58ce8aa6854570206688710f3e8ba40
Jay Vala <jay.vala@msn.com>
2018-11-30 12:47:43 +0100

    adding settings.py --> easily configurable script to not change the main code.

---
+++ flask-app/settings.py
@@ -0,0 +1,9 @@
+# Settings for the model
+
+MAX_LEN = 10  # maximum length a sentence can have
+
+BIN_MODEL_PATH = ''  # binary classifier model path
+
+CLUSTER_1_PATH = ''  # cluster 1 path
+
+CLUSTER_2_PATH = ''  # cluster 2 path
